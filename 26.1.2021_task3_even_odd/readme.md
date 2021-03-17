c8efb07f1f166d563f9582f2e2f7b755b94cd1bf
Dariwka <77488950+Dariwka@users.noreply.github.com>
2021-03-17 19:11:59 +0200

    Add files via upload

---
+++ 26.1.2021_task3_even_odd/readme.md
@@ -0,0 +1,5 @@
+#Even and Odd Task
+
+*if
+*alert
+\*Number function
---
+++ 26.1.2021_task3_even_odd/script.js
@@ -0,0 +1,4 @@
+var theNumber = Number(prompt("Please write one number"));
+if (theNumber % 2) {
+    alert("that is even number")
+} else alert("that number is odd")
